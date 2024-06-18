5f070878a87fde60db3364efd2166544059b87e0
Deepak <deepakworkpc@gmail.com>
2024-06-19 01:36:23 +0530

    using html-webpack-plugin, css-loader, style-loader to use css in the page and its all generated dynamically using the html-webpack-plugin inside the dist

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "src/index.html",
+        inject: "body"
+    })
+  ]
+};
